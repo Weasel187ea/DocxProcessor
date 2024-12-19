be7535e8a8cee88c7a187882a61894cc86e077b2
Axel Lopez <axellopez@Axels-Laptop.local>
2024-12-19 10:07:37 -0800

    decided to store data in dictionary format, also checks for bold words now

--- extractText.py
+++ extractText.py
@@ -2,6 +2,13 @@ import re
 import docx
 import os
 
+# from translate import Translator
+# translator= Translator(from_lang="german",to_lang="english")
+# # translation = translator.translate("Guten Morgen")
+
+
+def write_into_file(data): # write into file in csv format
+    pass
 
 
 def iter_headings(paragraphs):
@@ -14,46 +21,73 @@ def getText(filename):
     fullText = [] #all text will be joined at the end
     mainHeader = "" #header
     body = False #we have reached the body paragraph, collect everything
-    length = 0 # length of document
-    section = 0 # section stated on the document
-    Data = [] # --> [header, length, section, text]
+    Data = {} # --> [header, length, section, text]
 
 
-    count = 0
     for para in doc.paragraphs:
         if mainHeader == "": #this section extracts header
-            # print(f"this is the header:")
             for header in iter_headings(doc.paragraphs):
                 mainHeader = header.text
-                # print(mainHeader)
-            # print("end of header\n")
 
-        if para.text.startswith("Length"):
-            length = para.text.replace(u'\xa0', u' ') #gets rid of \xa0
-        elif para.text.startswith("Section"):
-            section = para.text.replace(u'\xa0', u' ') #gets rid of \xa0
-        elif para.text.startswith("Body"):
-            body = True
+        for run in para.runs:
+            if run.bold:
+                if "Section" in run.text:
+                    Data["section"] = para.text.replace(u'\xa0', u' ').replace("\n"," ")
+                elif "Length" in run.text:
+                    Data["length"] = para.text.replace(u'\xa0', u' ').replace("\n"," ")
+                elif "Body" in para.text:
+                    body = True
+
+
+        if para.text == "":
+            continue
+
+        # elif para.text.startswith("Copyright") or para.text.startswith("Load-Date"):
+        #     continue
+        elif para.text.startswith("CopyRight"):
+            continue
+        elif ("Load-Date:" in para.text) or ("End of Document" in  para.text):
+            continue
+
+
         elif body == True: #we are in the body paragraph
                 if (para.text != mainHeader) and (para.text != ""):
-                    fullText.append(para.text.replace("\n", " "))
+                    lineToAdd = para.text.replace("\n", " ")
+                    # fullText.append(re.sub('\s+', ' ', lineToAdd)) # look this over
+                    fullText.append(lineToAdd)
+        # else:
+        #     Data.append(para.text.replace(u'\xa0', u' ').replace("\n"," ")) #gets rid of \xa0
 
         
-    Data = [mainHeader, length, section, " ".join(fullText)]
+    Data["body"] = (" ".join(fullText))
     return Data
 
 
 
 if __name__ == "__main__":
+
+
     onlyfiles = os.listdir()
     FileData = []
 
     for file in onlyfiles:
         if file.endswith("docx"):
             FileData.append(getText(file))
+    # for data in FileData[0]:
+    #     print(data)
+    #     print()
 
-    for data in FileData:
-        for index in data:
-            print(index)
-            print()
-        break
+    print(FileData[0])
+    # for char in FileData[0][7]:
+    #     print(char, end="")
+    #     if char == ".":
+    #         print()
+
+
+    # for data in FileData:
+    #     # print(data)
+    #     for index in data:
+    #         print(index)
+    #         print()
+    #     break
+        
